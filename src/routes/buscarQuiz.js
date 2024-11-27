5bb673a79628c3b2cdcea55405f4376bc82dca1f
nicollyssousa <nicolly.sousa@sptech.school>
2024-11-27 01:11:07 -0300

    update API dataviz - kpis personagens funcionais

--- src/routes/buscarQuiz.js
+++ src/routes/buscarQuiz.js
@@ -7,4 +7,12 @@ router.post("/buscarQuiz/", function (req, res) {
     buscarQuizController.buscarQuizController(req, res);
 });
 
+router.post("/kpiNomePersonagem", function (req, res) {
+    buscarQuizController.kpiNomePersonagem(req, res);
+});
+
+router.post("/kpiPersonagemMaisPontuada", function (req, res) {
+    buscarQuizController.kpiPersonagemMaisPontuada(req, res);
+});
+
 module.exports = router;
